813e9b24b96cb02eabeae6b857077922103a1b99
Nicolas <nicolas.millarm@idiem.cl>
2025-07-19 20:59:50 -0400

    cree componente header y añadi animacion para que se desplace el contenido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RuletaComponent } from './components/ruleta/ruleta.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RuletaComponent],
+  imports: [RouterOutlet, RuletaComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
