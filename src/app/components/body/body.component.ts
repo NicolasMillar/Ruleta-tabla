c994e11c13b97bcade7eaddee5cf16169460e1b6
Nicolas <nicolas.millarm@idiem.cl>
2025-07-30 19:38:52 -0400

    añadiendo tabs

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { RuletaComponent } from '../ruleta/ruleta.component';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [RuletaComponent],
+  imports: [NgIf, RuletaComponent, TableComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
+  @Input() activeTab: 'ruleta' | 'tabla' = 'ruleta';
 
 }
